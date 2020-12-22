0fcc752723569d843043e176f260ba8676995fdb
Vincent-Snow <vincent.van.snow@gmail.com>
2020-12-22 08:35:40 -0700

    Solved Project Euler 14

---
+++ CollatzSequence/CollatzSequence/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  CollatzSequence
+//
+//  Created by Vincent Snow on 12/22/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var counter = 0
+var highest = (0,0)
+for i in 1...1000000 {
+    var x = i
+    counter = 0
+    //print(i, x)
+    while x != 1 {
+        if x % 2 == 0 {
+            x/=2
+        } else {
+            x = (3*x)+1
+        }
+        counter+=1
+    }
+    if counter > highest.1 {
+        highest = (i, counter)
+        print(highest)
+    }
+    
+}
+
