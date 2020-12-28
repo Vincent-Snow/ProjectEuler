5d9701f333991675d68e34970038df76df445d83
Vincent-Snow <vincent.van.snow@gmail.com>
2020-12-28 13:53:27 -0700

    Project Euler 48 solved!

--- Sum Powers/Sum Powers/main.swift
+++ Sum Powers/Sum Powers/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+var sum = 0
+for i in 1...1000 {
+    sum+=itself(x: i)
+    print(sum)
+}
+print(String(sum).suffix(10))
 
+func power(num:Int, powerOf:Int) -> Int {
+    var x = num
+    if num == 1 {
+        return 1
+    }
+    for _ in 1...powerOf-1 {
+        x*=num
+        if String(x).count > 11 {
+            let stringX = String(x).suffix(11)
+            x = Int(stringX)!
+        }
+    }
+    return x
+}
+
+func itself(x:Int) -> Int {
+    return power(num: x, powerOf: x)
+}
