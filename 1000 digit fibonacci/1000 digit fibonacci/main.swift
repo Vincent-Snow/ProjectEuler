3600918d58b8e8a1825f2a46830388919ff494ba
Vincent-Snow <vincent.van.snow@gmail.com>
2020-12-30 15:50:21 -0700

    Factorial Digit Sum solved

--- 1000 digit fibonacci/1000 digit fibonacci/main.swift
+++ 1000 digit fibonacci/1000 digit fibonacci/main.swift
@@ -6,6 +6,65 @@
 //
 
 import Foundation
+extension Double {
 
-print("Hello, World!")
+    func toString(decimal: Int = 9) -> String {
+        let value = decimal < 0 ? 0 : decimal
+        var string = String(format: "%.\(value)f", self)
 
+        while string.last == "0" || string.last == "." {
+            if string.last == "." { string = String(string.dropLast()); break}
+            string = String(string.dropLast())
+        }
+        return string
+    }
+}
+
+
+var fib: [String] = []
+var x = 1.0
+var y = 1.0
+var counter = -1
+var countHolder = 0
+var inARow = 0
+fib.append(x.toString())
+fib.append(y.toString())
+for _ in 1...737 {
+    //print(x, y)
+    x = x + y
+    y = x + y
+    fib.append(x.toString())
+    fib.append(y.toString())
+}
+//print(fib)
+for i in fib {
+    print(i)
+}
+var letsSee: [Int] = []
+countHolder = String(fib[0]).count
+for i in 0...fib.count-1 {
+    let x = String(fib[i]).count
+    //print(x)
+    if countHolder == x {
+        counter+=1
+    } else {
+
+        if counter == 5 {
+            inARow+=1
+        } else {
+            //print(inARow)
+            letsSee.append(inARow)
+            inARow = 0
+        }
+        print(String(fib[i-1]).count, counter)
+        countHolder = x
+        counter = 1
+    }
+}
+print(fib.last!.count, fib.count)
+var t = 0
+for x in letsSee {
+    t += (x*5)
+    t += 4
+}
+print(t)
--- Factorial Digit Sum/Factorial Digit Sum/main.swift
+++ Factorial Digit Sum/Factorial Digit Sum/main.swift
@@ -39,184 +39,45 @@ func factorial(a: Int) -> Int {
     }
 }
 
-//var x = 1.0
-//var xstr = ""
-//for i in 1...100 {
-//    x*=Double(i)
-//    xstr = String(x)
-//    while xstr.last! == "0" {
-//            xstr = String(xstr.dropLast())
-//            x = Double(xstr)!
-//    }
-//    print(i, x, xstr)
-//}
-//var t = xstr.map({Int(String($0)) ?? 0 }).reduce(0, +)
-//print(t)
-//var longNumArray: [String] = []
-
-//var tempArray: [String] = []
-//let twentyFact = 5*4*3*2*1
-//let twentyFact1 = 13*12*11*10*9*8*7*6*5*4*3*2*1
-
-//var x = 1
-//var fact = ""
-//var product = ""
-//print(twentyFact, twentyFact*x)
-//var f = String(1)
-var byWhat = 10
-//print(f.count/byWhat)
-//
-
-//let largeNum2 = "12341234123412341234123412341234"
-//var longNumArray2: [String] = []
-//var largeNumReversed2 = String(largeNum2.reversed())
-//let temp2 = largeNumReversed2.separate(every: 5, with: " ")
-//longNumArray2 = temp2.components(separatedBy: " ").map({String($0.reversed())}).reversed()
-//print(longNumArray2)
-
-var test = "888777"
-var result2 = 0
-var ovrflw = 0
-var fullanswer = ""
-func reverse(_ s: String) -> [String] {
-    var what: [String] = []
- for character in s {
-    //str = "\(character)" + str
-    what.insert(String(character), at: 0)
- }
- return what
-}
-var testrev = reverse(test)
-
-for i in testrev {
-    result2 = (Int(i)! * 8) + ovrflw
+func bigNum(strLongN:String, num:Int) -> String {
+    let longN = rev(s: strLongN)
+    var ar: [Int] = []
+    ovrflw = 0
     
-    if String(result2).count > 1 {
-        var first = String(result2)
-    } else {
-        ovrflw = 0
-    }
-    fullanswer+=result2
-}
-print(fullanswer, reverse(fullanswer))
-
-var overflow: Int = 0
-func multiplyByLargeNum(largeNum: String, multiplyBy: Int) -> String {
-    var longNumArray: [String] = []
-    var answerArray: [String] = []
-    let largeNumReversed = String(largeNum.reversed())
-    let temp = largeNumReversed.separate(every: byWhat, with: " ")
-    longNumArray = temp.components(separatedBy: " ").map({String($0.reversed())}).reversed()
-//    print(longNumArray)
-    var y = 1
-    var yAsString = ""
-    
-    overflow = 0
-    
-    for j in longNumArray.reversed() {
-            y = (multiplyBy * Int(j)!) + overflow
-            if y == 0 {
-                yAsString = "0000000000"
-            } else {
-                yAsString = String(y)
-            }
-            if yAsString.count > byWhat {
-                answerArray.insert(String(yAsString.suffix(byWhat)), at: 0)
-                overflow = Int(yAsString.prefix(yAsString.count-byWhat))!
-                if String(j) == longNumArray[0] {
-                    answerArray.insert(String(overflow), at: 0)
-                }
-            } else {
-                answerArray.insert(String(yAsString.suffix(byWhat)), at: 0)
-                overflow = 0
-            }
-//        print(answerArray)
-//        print(y)
+    for i in 0...longN.count-1 {
+        var prod = (longN[i] * num) + ovrflw
+        if prod >= 10 {
+            ovrflw = (prod - (prod % 10)) / 10
+            prod = prod % 10
+        } else {
+            ovrflw = 0
         }
-//    print(answerArray)
-    return answerArray.joined()
+        
+        ar.insert(prod, at: 0)
+        if i == longN.count-1 && ovrflw != 0{
+            ar.insert(ovrflw, at: 0)
+        }
+    }
+    let x = ar.map({String($0)}).joined()
+    return x
 }
 
-//print(multiplyByLargeNum(largeNum: "9426890448883247745626185743057242473809693764078951663494238777294707070023223798882976159207729119823605850588608460429412647567360000000000000000000000", multiplyBy: 99))
-
-var x = 1
-for i in 1...20 {
-    x*=i
-    print(i, x)
+func rev(s:String) -> [Int] {
+    var ar: [Int] = []
+    for char in s {
+        ar.insert(Int(String(char))!, at: 0)
+    }
+    return ar
 }
 
-var z = "1"
+func revInt(arr: [Int]) -> [Int] {
+    return arr.reversed() // we return here
+}
+
+var ovrflw = 0
+var ar1 = "1"
 for i in 1...100 {
-    z = multiplyByLargeNum(largeNum: z, multiplyBy: i)
-//    print(i, z)
+    ar1 = bigNum(strLongN: ar1, num: i)
+    print(ar1)
 }
-
-var fact100 = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000"
-//print(fact100)
-if z == fact100 {
-    print("yes")
-}
-print(z.reduce(0, {$0 + (Int(String($1)) ?? 0)}))
-
-
-//for i in 1...15 {
-//    tempArray = []
-//    if longNumArray.count > 0 {
-//        overflow = 0
-//        for j in longNumArray.reversed() {
-//            y = (i * Int(j)!) + overflow
-//            yAsString = String(y)
-//            if yAsString.count > 5 {
-//                tempArray.insert(String(yAsString.suffix(5)), at: 0)
-//                overflow = Int(yAsString.prefix(yAsString.count-5))!
-//            } else {
-//                tempArray.insert(String(yAsString.suffix(5)), at: 0)
-//                overflow = 0
-//            }
-//        }
-//        print(tempArray)
-//        longNumArray = tempArray
-//    } else {
-//        y*=i
-//        yAsString = String(y)
-//        longNumArray.append(yAsString)
-//    }
-//    print(longNumArray.joined())
-//}
-
-//let t = ["a","b","c","d","e"]
-//print(t.joined())
-//
-////for j in 1...20 {
-//    for i in 0...f.count/byWhat {
-//        //let other = f.prefix(f.count-(byWhat*i))
-//        //let first = other.suffix(byWhat)
-//        product = String((Int(first)!*x) + overflow)
-//        print(other, first)
-//        if product.count > first.count {
-//            overflow = Int(product.prefix(product.count - first.count))!
-//        }
-//
-//    }
-//    longNumArray.insert(String(product.suffix(byWhat)), at: 0)
-//    f = longNumArray.joined()
-//    print("length", f)
-////}
-//print("fact", fact, fact.count)
-//let intFact = fact.map({Int(String($0))!})
-//print(intFact.reduce(0,+))
-
-//factorial of 20 = 2432902008176640000
-
-
-
-//var firstThirteenArray: [Double] = []
-//let largeArray = input.components(separatedBy: "\n")
-//
-//for i in largeArray {
-//    let firstThirteen = String(i.prefix(13))
-//    firstThirteenArray.append(Double(firstThirteen) ?? 0)
-//}
-//
-//print(firstThirteenArray.reduce(0, +))
-
+print(ar1.reduce(0, {$0 + (Int(String($1)) ?? 0)}))
