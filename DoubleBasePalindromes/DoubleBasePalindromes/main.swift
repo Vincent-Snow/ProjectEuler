883c0b9016040bbe74ed472c879fcb8d48caac8d
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-26 09:03:37 -0700

    cleaned up

--- DoubleBasePalindromes/DoubleBasePalindromes/main.swift
+++ DoubleBasePalindromes/DoubleBasePalindromes/main.swift
@@ -8,17 +8,11 @@
 import Foundation
 
 let start = Date()
-//for i in 1...50 {
-//    let p = Int(pow(2.0, Double(i)))
-//    let s = String(p, radix: 2)
-//    print(s,p, i, s.count)
-//}
 var base: [Int:Bool] = [:]
 var sum = 0
 for i in 1...1000000 {
     if String(i) == String(String(i).reversed()) {
         base[i] = true
-        //print(i)
     }
 }
 for i in base.keys.sorted() {
